a37dea489c1391efe923bcdab5f7a61caabbc7ec
dboudro <dylanrboudro@gmail.com>
2015-10-12 23:41:51 -0400

    audio working

--- js/audio.js
+++ js/audio.js
@@ -1,24 +1,19 @@
-var meditationtrack1 = document.getElementById("meditation1");
-
-var meditationtrack2 = document.getElementById("meditation2");
-
-var meditationtrack3 = document.getElementById("meditation3");
 
 // event for hovering over first circle
 var playTrack1 = function() {
   var meditationtrack1 = document.getElementById("meditation1");
-  meditationtrack1.src = "./audio/breathe.mp3";
+  meditationtrack1.src = "./audio/kindness.mp3";
   meditationtrack1.play();
 }
 
-// event for hovering over second circle
-document.getElementById("circle2").mouseenter(function() {
+var playTrack2 = function() {
+  var meditationtrack2 = document.getElementById("meditation1");
+  meditationtrack2.src = "./audio/breathe.mp3";
+  meditationtrack2.play();
+}
 
-
-
-});
-
-// event for hovering over third circle
-document.getElementById("circle3").mouseenter(function() {
+var playTrack3 = function() {
+  var meditationtrack3 = document.getElementById("meditation1");
+  meditationtrack3.src = "./audio/body-scan.mp3";
   meditationtrack3.play();
-});
+}
